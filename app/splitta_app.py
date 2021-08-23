2ced244ba9a9b9c0122ffbd752f5f2c90d676ae7
sjjtu <johntu@live.de>
2021-08-23 07:26:01 +0200

    added currency conversion

--- app/splitta_app.py
+++ app/splitta_app.py
@@ -1,6 +1,9 @@
 import csv
 import time
 import os
+from forex_python. converter import CurrencyRates
+
+
 
 def create_split(name):
 	with open(f'{name}.txt', 'w') as f:
@@ -24,7 +27,9 @@ def print_split(file):
 	except FileNotFoundError as ferror:
 		print('please create a file first')
 
-def compute_balance(file):
+def compute_balance(file, target_currency='eur'):
+	c = CurrencyRates()
+	target_currency = target_currency.upper()
 	try:
 		with open(f'{file}.txt', 'r') as f:
 			reader=csv.reader(f)
@@ -32,28 +37,51 @@ def compute_balance(file):
 			balances = {}
 			next(reader, None)
 			for row in reader:
-				total+=int(row[1])
+				amount = c.convert(row[2], target_currency, int(row[1])) if row[2].upper() != target_currency else int(row[1])
+				total+= amount
 				if row[0] in balances:
-					balances[row[0]]+=int(row[1])
+					balances[row[0]]+= amount
 				else:
-					balances[row[0]]=int(row[1])
+					balances[row[0]]= amount
 
-			target = total/(len(balances))
 			balances = {person: amount for person, amount in sorted(balances.items(), key=lambda item: item[1])}
-			
-		
-
 		return balances, total
 						
 	except FileNotFoundError as ferror:
 		print('please create a file first')
 	
 
+def split(filename, target_currency='eur'):
+	balances, total = compute_balance(filename, target_currency)
+	target = total/(len(balances))
+	mutable_balances = balances.copy()
+	instructions = []
+	for person, amount in balances.items():
+		diff = target - amount
+		if diff <= 0:
+			break
+		for receiver, bonus in reversed(mutable_balances.items()):
+			if bonus - target == 0:
+				continue
+			if diff <= bonus - target:
+				instructions.append(f'{person} should give {receiver} {diff}')
+				mutable_balances[receiver] -= diff
+				break
+			else:
+				instructions.append(f'{person} should give {receiver} {bonus - target}')
+				mutable_balances[receiver] -= bonus - target
+				diff -= bonus - target
+	return instructions
+
 if __name__ == '__main__':
 	create_split('test')
-	add_split('test', 'john', 30)
-	add_split('test', 'john', 46)
-	add_split('test', 'wanda', '36')
-	add_split('test', 'asd	q', '36')
-	print_split('test')
+	add_split('test', 'john', 30, 'Sek')
+	add_split('test', 'john', 46, 'Sek')
+	add_split('test', 'wanda', '36', 'Sek')
+	add_split('test', 'simon', '38', 'Sek')
+	add_split('test', 'leni', '58', 'Sek')
+	add_split('test', 'thad', '34', 'Sek')
+	add_split('test', 'mel', '58', 'Sek')
+	#print_split('test')
 	print(compute_balance('test'))		
+	print(split('test'))
